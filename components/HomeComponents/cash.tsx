834e6a1628183673546f4d2e4853eb4e430df750
Ojefersoncode <jefersonvidal999@gmail.com>
2025-05-10 19:33:43 -0300

    Edit BattleBar

--- components/HomeComponents/cash.tsx
+++ components/HomeComponents/cash.tsx
@@ -31,51 +31,12 @@ export default function Cash() {
   const [showBalance, setShowBalance] = useState(true);
 
   return (
-    <div className="flex flex-col lg:flex-row bg-[#0e0e0e]">
-      {/* Card da Progresso e Ações */}
-      <div className="flex-1">
-        <Card className="border-none bg-transparent">
-          <CardHeader>
-            <CardTitle className="text-2xl text-green-100">
-              Bem vindo, sr Jeferson!
-            </CardTitle>
-          </CardHeader>
-
-          <CardContent>
-            <div className="mb-4">
-              <div className="flex justify-between text-green-50">
-                <div className="flex items-center gap-1 mb-2">
-                  <img
-                    src="/Level/bronze.svg"
-                    alt="patente"
-                    className="size-7 select-none touch-pan-down"
-                  />
-                  <span>Bronze nível 5</span>
-                </div>
-                <span>Nível 6</span>
-              </div>
-
-              <Progress value={28} className="h-4 bg-background">
-                <div className="absolute -bottom-6 left-[28%] transform -translate-x-1/2">
-                  <div className="w-0 h-0 border-l-[8px] border-l-transparent border-r-[8px] border-r-transparent border-b-[8px]" />
-                </div>
-              </Progress>
-
-              <div className="mt-4 flex justify-center">
-                <div className="bg-green-100 rounded-md p-2 font-bold text-xs text-green-900">
-                  Falta 72% para nível 6
-                </div>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
-      </div>
-
+    <div className="flex w-full bg-background">
       {/* Card do Saldo */}
       <Card className="w-full lg:w-[600px] px-0 shadow-none border-none bg-background">
-        <CardHeader className="flex flex-row justify-between pb-2 items-start text-green-100">
+        <CardHeader className="flex flex-row justify-between pb-2 items-start">
           <div className="flex items-center gap-2">
-            <CardTitle className="text-lg text-green-100">
+            <CardTitle className="text-lg text-text font-inter">
               Saldo disponível
             </CardTitle>
             <Eye
@@ -85,8 +46,8 @@ export default function Cash() {
           </div>
         </CardHeader>
 
-        <CardContent className="text-green-50">
-          <h1 className="text-4xl font-bold p-0 mb-4">
+        <CardContent className="text-text">
+          <h1 className="text-4xl font-titan p-0 mb-4">
             {showBalance ? '130.000,00' : '••••••••'}{' '}
             <span className="text-sm font-normal">pontos</span>
           </h1>
@@ -99,7 +60,7 @@ export default function Cash() {
             ].map((item, i) => (
               <div
                 key={i}
-                className="border border-green-500 bg-[#181818] hover:bg-[#212121] transition-all rounded-xl py-4 flex flex-col items-center justify-center cursor-pointer"
+                className="border bg-btn hover:bg-btn/40 transition-colors rounded-xl py-4 flex flex-col items-center justify-center cursor-pointer"
               >
                 <item.icon className="size-6 mb-4" />
                 <span className="text-xs font-bold text-white">
