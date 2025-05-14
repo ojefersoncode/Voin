3a31f0cc0a59f2894aae8179a16cba415e8ac1ad
Ojefersoncode <jefersonvidal999@gmail.com>
2025-05-14 15:55:06 -0300

    Edit Landing page navbar

--- components/landing/HeroLeads.tsx
+++ components/landing/HeroLeads.tsx
@@ -5,7 +5,7 @@ import { CaptureLeads } from './CaptureLeads';
 export const HeroLeads = () => {
   return (
     <section className="container grid lg:grid-cols-2 place-items-center bg-background py-10 max-md:gap-7">
-      <div className="text-center lg:text-start max-md:mx-4 space-y-4">
+      <div className="text-center lg:text-start space-y-4">
         <main className="text-2xl md:text-4xl font-titan">
           <h1 className="inline">
             <span className="inline bg-gradient-to-r text-gray-50">
