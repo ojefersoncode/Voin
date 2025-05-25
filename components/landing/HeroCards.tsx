3a6355ddd3466cef14efe40f354c5a773630a9e2
Ojefersoncode <jefersonvidal999@gmail.com>
2025-05-25 20:01:05 -0300

    Edit Landing page add style for page

--- components/landing/HeroCards.tsx
+++ components/landing/HeroCards.tsx
@@ -6,7 +6,7 @@ import { BlurFade } from '../magicui/blur-fade';
 export const HeroCards = () => {
   return (
     <div className="flex w-full justify-center items-center max-lg:mt-10 max-sm:mb-4">
-      <BlurFade className="bg-transparent" delay={0.25 * 0.05} inView>
+      <BlurFade className="bg-transparent" delay={0.2 * 0.03} inView>
         <Image
           src="/landing/Battlescreen.png" // Alterna as imagens conforme o tema
           alt="Voin"
--- components/landing/HeroLeads.tsx
+++ components/landing/HeroLeads.tsx
@@ -11,7 +11,7 @@ export const HeroLeads = () => {
   return (
     <section className="container w-full justify-center items-center grid lg:grid-cols-2 mb-4 py-10">
       <main className="flex flex-col w-full justify-center items-center sm:ml-10">
-        <BlurFade className="bg-transparent " delay={0.25 * 0.05} inView>
+        <BlurFade className="bg-transparent py-0" delay={0.2 * 0.5} inView>
           <div className="flex items-center justify-center text-center gap-3 px-3 rounded-full border border-gray-700 mb-2 bg-subbackground hover:bg-white/10 hover:shadow-lg drop-shadow-md shadow-white transition-colors duration-200">
             <Button className="flex justify-center items-center bg-transparent hover:bg-transparent p-0">
               <div className="flex items-center justify-center border-r h-full border-gray-700">
