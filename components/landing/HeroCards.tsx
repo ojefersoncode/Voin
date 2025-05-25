db0cd717f0dae34046b517453701d6ab2ce1289f
Ojefersoncode <jefersonvidal999@gmail.com>
2025-05-25 16:58:59 -0300

    Edit Landing page add style for page

--- components/landing/HeroCards.tsx
+++ components/landing/HeroCards.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image';
 
 export const HeroCards = () => {
   return (
-    <div className="flex w-full justify-center items-center">
+    <div className="flex w-full justify-center items-center max-sm:mt-10 max-sm:mb-4">
       <Image
-        src="/landing/Callimage.png" // Alterna as imagens conforme o tema
+        src="/landing/Battlescreen.png" // Alterna as imagens conforme o tema
         alt="Voin"
         height={2048}
         width={2048}
-        className="h-[500px]  object-contain"
+        className="h-[400px] max-sm:h-[600px] animate-in transition-all duration-500 w-fit border-2 border-gray-700 rounded-2xl object-contain"
       />
     </div>
   );
