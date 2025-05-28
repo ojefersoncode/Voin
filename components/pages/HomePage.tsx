38789a6c432e30ef1a9b04d10f13f706f9b69378
Ojefersoncode <jefersonvidal999@gmail.com>
2025-05-28 11:54:10 -0300

    Edit Balance component

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -14,7 +14,7 @@ export default function HomePage({ user }: { user: User }) {
       </header>
 
       <main className="flex flex-1 flex-col">
-        <div className="pb-2">
+        <div className="pb-4">
           <Balance />
         </div>
 
