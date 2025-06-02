b6182f4871ab9baceba810b79aa115b6ff2be3cb
Ojefersoncode <jefersonvidal999@gmail.com>
2025-06-02 15:07:34 -0300

    Edit Landing page

--- components/landing/CaptureLeads.tsx
+++ components/landing/CaptureLeads.tsx
@@ -160,10 +160,10 @@ export const CaptureLeads = () => {
           {loading ? 'Enviando cadastro...' : 'Quero entrar'}
         </Button>
       </form>
-      <div className="flex w-full items-center justify-center mt-10 gap-2 px-1">
+      <div className="flex w-full items-center justify-center mt-6 gap-2 px-1">
         <hr className="w-full border-btn" />
 
-        <span className="w-full text-nowrap text-text/70 font-inter lg:text-sm text-xs">
+        <span className="w-full text-nowrap text-text/70 font-inter lg:text-sm text-sm">
           Garanta seu acesso antecipado!
         </span>
 
