6c55ed1c50d6ca0b83dbea8cb4b80354c61c1129
Jeferson  Code <134572153+ojefersoncode@users.noreply.github.com>
2025-06-10 19:00:28 -0300

    Update Navbar.tsx

--- components/All/Navbar.tsx
+++ components/All/Navbar.tsx
@@ -7,7 +7,7 @@ export default function NavbarAll() {
     <>
       <header className="bg-background mb-2 touch-pan-x touch-pan-y">
         <nav className="flex items-center justify-between py-2 px-2">
-          <div className="flex items-center gap-1 sm:px-1 cursor-pointer">
+          <div className="flex items-center gap-1 sm:px-1 px-2 cursor-pointer">
             <img className="size-10" src="/Nexbattle.png" alt="Nexcoin" />
           </div>
 
