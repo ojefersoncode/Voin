2567f5d454f277bd6036004ec1d7420cef25ca21
Cooderfy <134572153+ojefersoncode@users.noreply.github.com>
2025-06-30 16:46:36 -0300

    Update Trading.tsx

--- components/TradingComponents/Trading.tsx
+++ components/TradingComponents/Trading.tsx
@@ -109,6 +109,10 @@ const handleChangeSaldo = (e: React.ChangeEvent<HTMLInputElement>) => {
 
 
           <div className="flex gap-4 items-center touch-pan-x touch-pan-y">
+            <div className"flex flex-col items-center justify-center">
+              <h1 className="text-sm font-semibold">Saldo</h1>
+              <h2 className="text-lg font-sans">BRL:1200,00</h2>
+            </div>
             <div className="bg-btn p-2 text-white rounded-lg">
               <Wallet className="w-5 h-5" />
             </div>
