e6aaf95026098e931cf18f9153d5d15127e827de
Cooderfy <134572153+ojefersoncode@users.noreply.github.com>
2025-06-30 17:29:41 -0300

    Update Trading.tsx

--- components/TradingComponents/Trading.tsx
+++ components/TradingComponents/Trading.tsx
@@ -27,29 +27,26 @@ export default function TradingAll() {
   const { toast } = useToast();
   const [selectedPair, setSelectedPair] = useState('BTCUSDT');
   const [inputValue, setInputValue] = useState<number>(0);
+  const [inputTempo, setInputTempo] = useState<number>(1); // Linha adicionada
+  const [inputSaldo, setInputSaldo] = useState<number>(10);
   const [isChartFullscreen] = useState(false);
 
   const tempoOptions = [
-  { label: '1 min', value: 1 },
-  { label: '5 min', value: 5 },
-  { label: '10 min', value: 10 },
-  { label: '15 min', value: 15 },
-  { label: '30 min', value: 30 },
-  { label: '1 hora', value: 60 },
-  { label: '12 horas', value: 720 },
-  { label: '24 horas', value: 1440 },
-];
+    { label: '1 min', value: 1 },
+    { label: '5 min', value: 5 },
+    { label: '10 min', value: 10 },
+    { label: '15 min', value: 15 },
+    { label: '30 min', value: 30 },
+    { label: '1 hora', value: 60 },
+    { label: '12 horas', value: 720 },
+    { label: '24 horas', value: 1440 },
+  ];
 
-// Estados
-const [inputTempo, setInputTempo] = useState<number>(1);
-const [inputSaldo, setInputSaldo] = useState<number>(10);
-
-const handleChangeSaldo = (e: React.ChangeEvent<HTMLInputElement>) => {
-  const value = e.target.value.replace(/\D/g, '');
-  const numericValue = Number(value);
-  setInputSaldo(numericValue < 1 ? 1 : numericValue);
-};
-  
+  const handleChangeSaldo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\D/g, '');
+    const numericValue = Number(value);
+    setInputSaldo(numericValue < 1 ? 1 : numericValue);
+  };
 
   const handleUpOperation = () => {
     const now = new Date();
@@ -90,28 +87,26 @@ const handleChangeSaldo = (e: React.ChangeEvent<HTMLInputElement>) => {
   return (
     <div className="bg-background h-dvh text-white flex flex-col touch-pan-x touch-pan-y">
       {/* Header */}
-
       <header className="bg-background top-0 z-30 touch-pan-x touch-pan-y">
         <nav className="flex items-center justify-between px-2 py-4">
           <div className="flex justify-between items-center touch-pan-x touch-pan-y">
-              <Select value={selectedPair} onValueChange={setSelectedPair}>
-                <SelectTrigger className="bg-subbackground text-text py-1.5 px-3 font-titan rounded-lg border-none h-10">
-                  <SelectValue placeholder="Selecione um par" />
-                </SelectTrigger>
-                <SelectContent className="bg-background text-white border border-gray-700">
-                  {availablePairs.map((pair) => (
-                    <SelectItem
-                      className="font-inter hover:bg-subbackground hover:text-black focus:bg-subbackground focus:text-text"
-                      key={pair.value}
-                      value={pair.value}
-                    >
-                      {pair.label}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
-
+            <Select value={selectedPair} onValueChange={setSelectedPair}>
+              <SelectTrigger className="bg-subbackground text-text py-1.5 px-3 font-titan rounded-lg border-none h-10">
+                <SelectValue placeholder="Selecione um par" />
+              </SelectTrigger>
+              <SelectContent className="bg-background text-white border border-gray-700">
+                {availablePairs.map((pair) => (
+                  <SelectItem
+                    className="font-inter hover:bg-subbackground hover:text-black focus:bg-subbackground focus:text-text"
+                    key={pair.value}
+                    value={pair.value}
+                  >
+                    {pair.label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
 
           <div className="flex gap-4 items-center touch-pan-x touch-pan-y">
             <div className="p-2 rounded-lg border border-zinc-400 dark:border-border/80">
@@ -125,82 +120,79 @@ const handleChangeSaldo = (e: React.ChangeEvent<HTMLInputElement>) => {
               <Wallet className="w-5 h-5" />
             </div>
             <div className="p-2 rounded-full text-black bg-white">
-           <UserRound className="w-5 h-5" />
-          </div>
+              <UserRound className="w-5 h-5" />
+            </div>
           </div>
         </nav>
       </header>
 
       {/* Main */}
       <div
-        className={`flex flex-col lg:flex-row w-full flex-1 sm:mb-2 touch-pan-x touch-pan-y ${isChartFullscreen ? 'lg:flex-col' : ''}`}
+        className={`flex flex-col lg:flex-row w-full flex-1 sm:mb-2 touch-pan-x touch-pan-y ${
+          isChartFullscreen ? 'lg:flex-col' : ''
+        }`}
       >
         {/* Chart */}
-        <div
-          className={`w-full lg:flex-1 relative max-sm:h-full max-sm:px-0 md:px-2 px-4`}
-        >
+        <div className="w-full lg:flex-1 relative max-sm:h-full max-sm:px-0 md:px-2 px-4">
           <TradingViewWidget symbol={selectedPair} />
         </div>
 
         {/* Trading Panel */}
-   <div className={`lg:w-[350px] w-full flex flex-col bg-background`}>
-    
-{/* Trading Controls */}
- <div className=" max-md:pt-4 touch-pan-x touch-pan-y">
-    <div className="flex w-full items-center gap-3">
-      {/* Input de Tempo */}
-        <div className="touch-pan-x touch-pan-y">
-         {/* Select de Tempo com tema atual */}
-<div className="flex flex-col gap-1 bg-subbackground rounded-xl px-4 py-3 w-full text-white">
-  <span className="text-sm text-text/60">Tempo</span>
-  <Select
-    className="bg-subbackground"
-    value={inputTempo.toString()}
-    onValueChange={(value) => setInputTempo(Number(value))}
-  >
-    <SelectTrigger className="bg-background text-white border-none rounded-md h-10 px-3">
-      <SelectValue placeholder="Selecione o tempo" />
-    </SelectTrigger>
-    <SelectContent className="bg-subbackground text-white border border-border">
-      {tempoOptions.map((option) => (
-        <SelectItem
-          key={option.value}
-          value={option.value.toString()}
-          className="hover:bg-background hover:text-white cursor-pointer"
-        >
-          {option.label}
-        </SelectItem>
-      ))}
-    </SelectContent>
-  </Select>
-</div>
-  </div>
+        <div className="lg:w-[350px] w-full flex flex-col bg-background">
+          {/* Trading Controls */}
+          <div className="max-md:pt-4 touch-pan-x touch-pan-y">
+            <div className="flex items-center gap-3">
+              {/* Input de Tempo */}
+              <div className="touch-pan-x touch-pan-y">
+                <div className="flex flex-col gap-1 bg-subbackground rounded-xl px-4 py-3 w-full text-white">
+                  <span className="text-sm text-text/60">Tempo</span>
+                  <Select
+                    value={inputTempo.toString()}
+                    onValueChange={(value) => setInputTempo(Number(value))}
+                  >
+                    <SelectTrigger className="bg-background text-white border-none rounded-md h-10 px-3">
+                      <SelectValue placeholder="Selecione o tempo" />
+                    </SelectTrigger>
+                    <SelectContent className="bg-subbackground text-white border border-border">
+                      {tempoOptions.map((option) => (
+                        <SelectItem
+                          key={option.value}
+                          value={option.value.toString()}
+                          className="hover:bg-background hover:text-white cursor-pointer"
+                        >
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
 
-  {/* Input de Saldo */}
-  <div className="touch-pan-x touch-pan-y">
-<div className="bg-subbackground rounded-xl px-4 py-2 flex flex-col items-center text-center">
-  <span className="text-sm text-text/60 ">Valor</span>
-  <div className="flex items-center gap-4 justify-between w-full">
-    <button
-      onClick={() => setInputSaldo((prev) => Math.max(1, prev - 1))}
-      className="text-btn hover:text-btn/80 transition-colors"
-    >
-      <Minus size={20} />
-    </button>
-    <span className="text-xl font-bold text-white">R${inputSaldo}</span>
-    <button
-      onClick={() => setInputSaldo((prev) => prev + 1)}
-      className="text-btn hover:text-btn/80 transition-colors"
-    >
-      <Plus size={20} />
-    </button>
-  </div>
-</div>
-  </div>
- </div>
-              
+              {/* Input de Saldo */}
+              <div className="touch-pan-x touch-pan-y">
+                <div className="bg-subbackground rounded-xl px-4 py-2 flex flex-col items-center text-center">
+                  <span className="text-sm text-text/60">Valor</span>
+                  <div className="flex items-center gap-4 justify-between w-full">
+                    <button
+                      onClick={() => setInputSaldo((prev) => Math.max(1, prev - 1))}
+                      className="text-btn hover:text-btn/80 transition-colors"
+                    >
+                      <Minus size={20} />
+                    </button>
+                    <span className="text-xl font-bold text-white">R${inputSaldo}</span>
+                    <button
+                      onClick={() => setInputSaldo((prev) => prev + 1)}
+                      className="text-btn hover:text-btn/80 transition-colors"
+                    >
+                      <Plus size={20} />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
             {/* Action Buttons */}
-            <div className="flex w-full items-center justify-between  gap-2 mt-4 mb-2 touch-pan-x touch-pan-y">
+            <div className="flex w-full items-center justify-between gap-2 mt-4 mb-2 touch-pan-x touch-pan-y">
               <button
                 onClick={handleDownOperation}
                 className="bg-[#dd3240] flex-1 hover:bg-[#af2732] transition-colors duration-200 text-text hover:text-text/80 py-3 px-4 rounded-lg font-medium flex items-center justify-between"
@@ -212,7 +204,7 @@ const handleChangeSaldo = (e: React.ChangeEvent<HTMLInputElement>) => {
               <div className="">
                 <Order />
               </div>
-              
+
               <button
                 onClick={handleUpOperation}
                 className="bg-[#097261] flex-1 hover:bg-[#135a4e] transition-colors duration-200 text-text hover:text-text/80 py-3 px-4 rounded-lg font-medium flex items-center justify-between"
