b4bc1589cf293f849d2dc4d5994e614bf5cebe83
Cooderfy <134572153+ojefersoncode@users.noreply.github.com>
2025-07-02 02:44:02 -0300

    Update BalanceButton.tsx

--- components/All/BalanceButton.tsx
+++ components/All/BalanceButton.tsx
@@ -13,10 +13,10 @@ export default function BalanceButton() {
     <>
        <Button 
          onClick={navigateToStore}
-          className="p-0 border-none outline-none rounded-lg bg-subbackground hover:bg-subbackground">
+          className="border-none outline-none rounded-lg bg-subbackground hover:bg-subbackground">
             <div className="flex w-full text-text justify-center items-center gap-2 p-2">
-              <img className="size-6" src="/Coin/Nexcoin.png" alt="logo" />
-              <h1 className="font-inter text-text/90 text-base">
+              <img className="size-5" src="/Coin/Nexcoin.png" alt="logo" />
+              <h1 className="font-inter text-text/90 text-sm">
                 130.000.00
               </h1>
             </div>
