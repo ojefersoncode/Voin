41d8feed7259cd2f5d69b29b4f92ed9dbb79b3a7
ojefersoncode <Jefersonvidal999@gmail.com>
2025-08-30 16:48:42 -0300

    Edit Trading page

--- components/All/Navbar.tsx
+++ components/All/Navbar.tsx
@@ -1,15 +1,41 @@
-import { Dessert } from 'lucide-react';
-import ButtonMenu from './ButtonMenu';
+import { Dessert, Menu, Wallet } from 'lucide-react';
+
+import { Button } from '../ui/button';
 
 export default function NavbarAll() {
   return (
     <>
-      <header className="bg-background border-b border-zinc-700 touch-pan-x touch-pan-y">
-        <nav className="flex items-center justify-between px-4 py-2">
-          <div className="flex items-center gap-1 sm:px-1 cursor-pointer">
-            <img className="size-8" src="/Nexbattle.png" alt="Nexcoin" />
+      <header className="bg-blackground top-0 z-30 touch-pan-x touch-pan-y border-b border-zinc-700 mb-3">
+        <nav className="flex items-center justify-between px-2 py-2">
+          <div className="flex gap-4 items-center touch-pan-x touch-pan-y">
+            <img
+              className="h-8 w-8 max-md:h-7 max-md:w-7"
+              src="/Bronk.png"
+              alt="logo"
+            />
           </div>
-            <ButtonMenu />
+
+          <div className="flex items-center gap-4">
+            <div className="flex gap-2 items-center rounded-lg touch-pan-x touch-pan-y">
+              <div className="flex items-center rounded-sm bg-btn">
+                <div className="flex flex-col items-start rounded-l-sm bg-black/30 py-1 px-2 justify-center">
+                  <div className="flex w-full items-center m-0 p-0 text-text">
+                    <span className="text-xs font-semibold">Seus coins</span>
+                  </div>
+                  <div className="flex items-center gap-1 text-sm text-text font-inter m-0 p-0">
+                    1200.00
+                  </div>
+                </div>
+                <Button className="bg-btn px-2 py-0 dark:bg-btn hover:bg-btn dark:hover:bg-btn">
+                  <Wallet className="size-5" />
+                </Button>
+              </div>
+            </div>
+
+            <Button className="p-0 text-text dark:text-text">
+              <Menu className="size-6" />
+            </Button>
+          </div>
         </nav>
       </header>
     </>
--- components/pages/Torneios.tsx
+++ components/pages/Torneios.tsx
@@ -2,10 +2,14 @@
 
 import { User } from '@supabase/supabase-js';
 import Tournament from '../Tournarmeint/Torneio';
+import NavbarAll from '../All/Navbar';
 
 export default function Torneios({ user }: { user: User }) {
   return (
     <div className="flex-1 overflow-hidden flex flex-col bg-background touch-pan-x touch-pan-y">
+      <div>
+        <NavbarAll />
+      </div>
       <Tournament />
     </div>
   );
